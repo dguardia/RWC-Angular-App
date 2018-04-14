fd6efae6a7cc29f807613252efeb323ee41869d6
DAVID GUARDIA <david.guardia@gmail.com>
2018-04-14 01:45:05 -0400

    adding the chart

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import {AngularFireDatabase} from 'angularfire2/database';
 import * as $ from 'jquery';
+import 'easy-pie-chart/dist/jquery.easypiechart.js';
+import * as EasyPieChart from 'easy-pie-chart/dist/jquery.easypiechart.js';
 
 @Component({
   selector: 'app-skills',
@@ -11,10 +13,13 @@ import * as $ from 'jquery';
 export class SkillsComponent implements OnInit {
   title = 'My skills';
   skills: Observable<any[]>;
+  newSkills: any[];
   constructor(db: AngularFireDatabase) {
     this.skills = db.list('/skills').valueChanges();
+    this.skills.subscribe(newSkills => {
+      this.newSkills = newSkills;
+    });
   }
-
   ngOnInit() {
     $(document).ready(function () {
       $('button').on('click', function () {
@@ -22,7 +27,17 @@ export class SkillsComponent implements OnInit {
         div.animate({left: '100px'}, 'slow');
         div.animate({fontSize: '5em'}, 'slow');
       });
+      $('.chart').easyPieChart({
+        scaleColor: '#ecf0f1',
+        lineWidth: 20,
+        lineCap: 'butt',
+        barColor: '#1abc9c',
+        trackColor:	'#ecf0f1',
+        size: 160,
+        animate: 500
+      });
     });
   }
+
 
 }
